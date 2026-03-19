8278b532b422071e045641b80b0a0585fc04eabe
Jaka Kovač <jaka.kovac@noordunglabs.si>
2026-03-19 12:01:49 +0100

    Deploy website - based on 1292699289e0b723980d32be0ead7b859902cb7b

--- assets/js/c15d9823.9aac3d23.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkvegasat=globalThis.webpackChunkvegasat||[]).push([[146],{9328:a=>{a.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":7,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
